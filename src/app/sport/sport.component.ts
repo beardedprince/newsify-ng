cbfe80df2eaeb3fd84655b02a7cb70597131b076
beardedprince <alaketuolamilekan02@gmail.com>
2020-01-14 23:54:38 +0100

    add meta data and titles

--- src/app/sport/sport.component.ts
+++ src/app/sport/sport.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import * as AOS from 'aos';
+import {Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sport',
@@ -11,9 +12,16 @@ export class SportComponent implements OnInit {
 
   posts: object;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, private title: Title, private meta: Meta) { }
 
   ngOnInit() {
+    this.title.setTitle('Newsify - Sport');
+    this.meta.addTags([
+      {name: 'title', content: 'Newsify' },
+      {name: 'description', content: 'Newsify app feed bring news feed from different source to you'},
+      {name: 'keywords', content: 'news, news feeds, feeds, daily news, sport news, health news, science and tech news'}
+
+    ]);
     this.data.getPosts().subscribe(data => {
       this.posts = data;
       console.log(this.posts);
